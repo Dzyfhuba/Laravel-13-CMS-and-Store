2d4e26ebd8f92af2f58d2b7231af1c1598fb4d8e
Hafidz Ubaidillah <hafidz21ub@gmail.com>
2026-08-13 15:36:17 +0700

    feat: add header nav desktop

--- resources/js/pages/Welcome.svelte
+++ resources/js/pages/Welcome.svelte
@@ -11,7 +11,22 @@
 <AppHead></AppHead>
 
 <header>
-    navigasi
+    <nav class="h-14 py-3 px-6 flex justify-between fixed top-0 bg-base-100 w-full">
+        <a href="/#">
+            <img
+                src="/hafidz-ubaidillah-logo.png"
+                alt="hafidz ubaidillah logo"
+                class="h-full"
+            />
+        </a>
+
+        <div class="flex">
+            <a href="#products" class="px-5 hover:underline">Produk</a>
+            <a href="#services" class="px-5 hover:underline">Jasa</a>
+            <a href="#projects" class="px-5 hover:underline">Proyek</a>
+            <a href="#blogs" class="px-5 hover:underline">Blog</a>
+        </div>
+    </nav>
 </header>
 <section id="onboarding" class="min-h-screen bg-amber-950">
     <p>berisi pemancing</p>
@@ -22,15 +37,14 @@
 <section id="product" class="min-h-screen bg-green-950">
     <p>onboarding</p>
 </section>
-<section id="onboarding" class="min-h-screen bg-red-950">
+<section id="services" class="min-h-screen bg-red-950">
     <p>services short list</p>
 </section>
-<section id="onboarding" class="min-h-screen bg-cyan-950">
+<section id="projects" class="min-h-screen bg-cyan-950">
     <p>projects short list</p>
 </section>
-<section id="onboarding" class="min-h-screen bg-indigo-950">
+<section id="blogs" class="min-h-screen bg-indigo-950">
     <p>blogs short list</p>
 </section>
-<footer id="footer" class="min-h-screen bg-indigo-950">
-footer
-</footer>
+<section id="affiliate" class="min-h-screen bg-indigo-950">afiliasi</section>
+<footer id="footer" class="min-h-screen bg-indigo-950">footer</footer>
